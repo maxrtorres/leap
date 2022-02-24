2cb8b6def09ddb418675aef798a0976514ddc5b3
Max Torres <maxtorres487@gmail.com>
2022-02-24 12:03:08 -0500

    Pause and resume game from background

--- src/game/utils/Utils.js
+++ src/game/utils/Utils.js
@@ -1,20 +1,5 @@
-import {Dimensions, Alert} from 'react-native';
-import {Entities} from '../../entities';
+import {Dimensions} from 'react-native';
 
 export const getDimensions = () => {
   return Dimensions.get('screen');
-};
-
-export const gameOver = gameRef => {
-  gameRef.stop();
-  Alert.alert('Game over!', 'Play again?', [
-    {
-      text: 'Play',
-      onPress: () => {
-        gameRef.swap(Entities());
-        gameRef.start();
-      },
-      style: 'default',
-    },
-  ]);
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 import React, {PureComponent} from 'react';
-import {StyleSheet, StatusBar} from 'react-native';
+import {StyleSheet, StatusBar, AppState, Alert} from 'react-native';
 import {GameEngine} from 'react-native-game-engine';
 import {Physics} from './game/systems/physics';
 import {Jump} from './game/systems/jump';
 import {Obstacle} from './game/systems/obstacle';
 import {Entities} from './entities';
-import {gameOver} from './game/utils/Utils';
 
 class App extends PureComponent {
   constructor() {
     super();
+  }
+
+  state = {
+    appState: AppState.currentState,
+    isRunning: true,
+  };
+
+  componentDidMount() {
+    this.appStateSubscription = AppState.addEventListener(
+      'change',
+      nextAppState => {
+        if (
+          this.state.isRunning &&
+          this.state.appState.match(/inactive|background/) &&
+          nextAppState === 'active'
+        ) {
+          this.refs.gameEngine.start();
+        } else if (
+          this.state.isRunning &&
+          this.state.appState.match(/inactive|active/) &&
+          nextAppState === 'background'
+        ) {
+          this.refs.gameEngine.stop();
+        }
+        this.setState({appState: nextAppState});
+      },
+    );
+  }
+
+  componentWillUnmount() {
+    this.appStateSubscription.remove();
   }
 
   render() {
@@ -22,7 +52,19 @@ class App extends PureComponent {
         onEvent={e => {
           switch (e) {
             case 'game-over':
-              gameOver(this.refs.gameEngine);
+              this.setState({isRunning: false});
+              this.refs.gameEngine.stop();
+              Alert.alert('Game over!', 'Play again?', [
+                {
+                  text: 'Play',
+                  onPress: () => {
+                    this.refs.gameEngine.swap(Entities());
+                    this.setState({isRunning: true});
+                    this.refs.gameEngine.start();
+                  },
+                  style: 'default',
+                },
+              ]);
           }
         }}>
         <StatusBar hidden={true} />
