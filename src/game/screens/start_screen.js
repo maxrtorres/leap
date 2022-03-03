4cff3ce6a1134d12d92b9e18dd1beda38efda1b5
Max Torres <maxtorres487@gmail.com>
2022-03-03 18:13:10 -0500

    Add custom font

--- src/game/screens/start_screen.js
+++ src/game/screens/start_screen.js
@@ -87,14 +87,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 0.2 * width,
     color: Colors.textColor,
-    fontFamily: 'sans-serif-condensed',
+    fontFamily: 'Pixel',
     marginBottom: 0.05 * height,
   },
   highscore: {
     textAlign: 'center',
     fontSize: 0.05 * width,
     color: Colors.textColor,
-    fontFamily: 'sans-serif-condensed',
+    fontFamily: 'Pixel',
   },
   image: {
     width: width / 2,
@@ -106,7 +106,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 0.05 * width,
     color: Colors.textColor,
-    fontFamily: 'sans-serif-condensed',
+    fontFamily: 'Pixel',
   },
 });
 
