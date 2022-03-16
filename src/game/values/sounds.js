b4a128f12a89bedcffcd6e43be88c1cbf88e8762
Max Torres <maxtorres487@gmail.com>
2022-03-16 14:33:12 -0400

    Increase sound volume

--- src/game/values/sounds.js
+++ src/game/values/sounds.js
@@ -5,7 +5,7 @@ export const startMusic = new Sound('start.ogg', Sound.MAIN_BUNDLE, error => {
     console.log('start music', error);
   } else {
     startMusic.setNumberOfLoops(-1);
-    startMusic.setVolume(0.7);
+    startMusic.setVolume(1);
     startMusic.play();
   }
 });
@@ -15,7 +15,7 @@ export const gameMusic = new Sound('game.wav', Sound.MAIN_BUNDLE, error => {
     console.log('game music', error);
   } else {
     gameMusic.setNumberOfLoops(-1);
-    gameMusic.setVolume(0.2);
+    gameMusic.setVolume(1);
   }
 });
 
@@ -23,7 +23,7 @@ export const jumpSound = new Sound('jump.wav', Sound.MAIN_BUNDLE, error => {
   if (error) {
     console.log('jump sound', error);
   } else {
-    jumpSound.setVolume(0.1);
+    jumpSound.setVolume(0.5);
   }
 });
 
@@ -31,6 +31,6 @@ export const loseSound = new Sound('lose.wav', Sound.MAIN_BUNDLE, error => {
   if (error) {
     console.log('lose sound', error);
   } else {
-    loseSound.setVolume(0.1);
+    loseSound.setVolume(0.5);
   }
 });
